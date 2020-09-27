9e89533e747d9647d8fb747293ed74936c5ed409
phuongnguyen1998611 <71966669+phuongnguyen1998611@users.noreply.github.com>
2020-09-27 17:06:46 +0700

    Add files via upload

---
+++ demoFunction.py
@@ -0,0 +1,9 @@
+#nhan_doi = lambda a:a*2
+#print(nhan_doi(4))
+
+danh_sach = [1,2,3,4,5,6,7,8,9,10]
+tap_hop = filter(lambda a:a%2==0, danh_sach)
+print(list(tap_hop))
+
+tap_hop_nhan_doi = list(map(lambda a:a*2, danh_sach))
+print(tap_hop_nhan_doi)
